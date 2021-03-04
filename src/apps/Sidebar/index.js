c496661a686d68b33d38a6fd11f8014d0f23e1ef
laura <wiu19186@lehre.dhbw-stuttgart.de>
2021-03-04 19:56:47 +0100

    Deleted submenu Create Survey

--- src/apps/Sidebar/index.js
+++ src/apps/Sidebar/index.js
@@ -1,10 +1,6 @@
 import "./style.css"
 import { CgMenu, CgClose } from "react-icons/cg"
 import { useRef, useState } from "react"
-import {
-    BrowserRouter as Router,
-    Link
-} from "react-router-dom"
 
 
 export default Sidebar
@@ -21,24 +17,16 @@ function Sidebar() {
     }
 
     return (
-        <Router>
         <div className="sidebar">
             <div className="menu-toggle item activatable"><button className="item" onClick={ toggleMenu }>{ showMenu ? <CgClose className="icon"/> : <CgMenu className="icon"/> }</button></div>
+
             <div className="side-menu" ref={ sideMenuRef }>
-
-                <div className="item activatable"><Link to="/">Home</Link></div>
-                <div className="item activatable"><Link to="/create-survey">Create Survey</Link></div>
-
-                        <div className="item activatable level"><Link to="/survey-type">Survey Type</Link></div>
-                        <div className="item activatable level"><Link to="/details">Details</Link></div>
-                        <div className="item activatable level"><Link to="/customize">Customize</Link></div>
-                        <div className="item activatable level"><Link to="/implement">Implement</Link></div>
-
-                <div className="item activatable"><Link to="/view-results">View Results</Link></div>
-                <div className="item activatable"><Link to="/my-account">My Account</Link></div>
-                <div className="item activatable"><Link to="/get-help">Get Help</Link></div>
+                <div className="item activatable"><a href={ `${process.env.REACT_APP_WEBSITE_URL}/` }>Home</a></div>
+                <div className="item activatable"><a href={ `${process.env.REACT_APP_WEBSITE_URL}/create-survey` } >Create Survey</a></div>
+                <div className="item activatable"><a href={ `${process.env.REACT_APP_WEBSITE_URL}/view-results` }>View results</a></div>
+                <div className="item activatable"><a href={ `${process.env.REACT_APP_WEBSITE_URL}/my-account` }>My Account</a></div>
+                <div className="item activatable"><a href={ `${process.env.REACT_APP_WEBSITE_URL}/get-help` }>Get help</a></div>
             </div>
         </div>
-        </Router>
     )
 }
