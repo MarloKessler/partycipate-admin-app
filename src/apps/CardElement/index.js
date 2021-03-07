a0eff8a15cd968e587a90ea9bdef809827392966
Melanie-31 <melanie-rudloff@gmx.de>
2021-03-07 19:45:58 +0100

    Update ChooseSurveyType, Style components

--- src/apps/CardElement/index.js
+++ src/apps/CardElement/index.js
@@ -4,10 +4,11 @@ import "./style.css"
 export default CardElement
 
 
-function CardElement({ children, className = "", ...props }){
+function CardElement({ children, content, className = "", ...props }){
     return(
         <div className={ `CardElement ${className}` } {...props}>
             { children }
+            { content }
         </div>
     )
 }
--- src/apps/CreateSurveyView/SelectSurveyComponent/index.js
+++ src/apps/CreateSurveyView/SelectSurveyComponent/index.js
@@ -1,14 +1,18 @@
 import "./style.css"
 import CardElement from "../../CardElement"
 import ESComponentContainer from "../ESComponentContainer"
+import { XCoiceBlock } from "../EditSurveyComponent/survey-elements/QuestionElement/content-blocks/XCoiceBlock"
 
 export default SelectSurveyComponent
 
 function SelectSurveyComponent() {
     return(
-        <ESComponentContainer className="select-survey-component" title="Create your survey:">
-            <CardElement content="Single-choice"></CardElement>
-            <CardElement content="Multiple-choice"></CardElement>
+        <ESComponentContainer className="select-survey-component" title="Choose your survey type:">
+            <div className="cards-hover">
+                <CardElement className="cards" content="Single-choice" onClick={XCoiceBlock}></CardElement>
+                <CardElement className="cards" content="Multiple-choice" onClick={XCoiceBlock}></CardElement>
+                <CardElement className="cards" content="Star Valuation"></CardElement>
+            </div>
         </ESComponentContainer>
     )
 }
