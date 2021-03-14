05442dda1cd7d4dc36dcf488b4017326346095a8
Melanie-31 <melanie-rudloff@gmx.de>
2021-03-14 21:36:02 +0100

    Logo, Design, Update Sidebar/Survey-/Resultview

--- src/apps/PageTitleElement/index.js
+++ src/apps/PageTitleElement/index.js
@@ -2,22 +2,22 @@ import "./style.css";
 import React,{ Component } from 'react';
 
 
-class PageTitleElement extends Component {
 
-    render(){
-        return(
-            <div>
-                <h1 className="PageTitle">{ this.props.children }</h1>
-                <hr style={{
-                    color: "black",
-                    backgroundColor: "black",
-                    height: 0.5,
-                    marginLeft: 20,
-                    width: 60
-                }}/>
-            </div>
-        )
-    }
+
+function PageTitleElement({ children, className = "" }) {
+
+    return(
+        <div className={ `page-title-element ${className}` }>
+            <h1 className="PageTitle">{ children }</h1>
+            <hr style={{
+                color: "black",
+                backgroundColor: "black",
+                height: 0.5,
+                marginLeft: 20,
+                width: 60
+            }}/>
+        </div>
+    )
 }
 
 export default PageTitleElement
--- src/apps/ResultsView/QuestionResultsElement/index.js
+++ src/apps/ResultsView/QuestionResultsElement/index.js
@@ -1,4 +1,5 @@
-//import "./style.css"
+import "./style.css"
+import CardElement from "../../CardElement"
 import DoughnutChart from "./charts/DoughnutChart"
 import BarChart from "./charts/BarChart"
 
@@ -7,31 +8,37 @@ export default QuestionResultsElement
 
 
 function QuestionResultsElement({ element }) {
-  console.log(element)
     return (
-        <div className="results-element">
-            <h3>{ element.content.question }</h3>
+        <CardElement className="results-element">
+            <div className="re-header">
+                <h3>{ element.content.question }</h3>
+                <h4></h4>
+            </div>
             <p><small>{ getElementTypeLabel(element.type) }</small></p>
             <ResultsChart element={ element } />
-            <p>kjhtkvdhsf</p>
-        </div>
+        </CardElement>
     )
 }
 
 
+const getTotalVotes(element) {
+    
+}
+
+
 function getElementTypeLabel(type) {
-  switch (type) {
-      case "single-choice": return "Single Choice"
-      case "multiple-choice": return "Multiple Choice" 
-      default: return ""
-  }
+    switch (type) {
+        case "single-choice": return "Single Choice"
+        case "multiple-choice": return "Multiple Choice" 
+        default: return ""
+    }
 }
 
 
 function ResultsChart({ element }) {
-  switch (element.type) {
-      case "single-choice": return <DoughnutChart element={ element }/>
-      case "multiple-choice": return <BarChart element={ element }/>
-      default: <div>no chart type match</div>
-  }
+    switch (element.type) {
+        case "single-choice": return <DoughnutChart element={ element }/>
+        case "multiple-choice": return <BarChart element={ element }/>
+        default: <div>no chart type match</div>
+    }
 }
