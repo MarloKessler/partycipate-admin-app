332233d711d7b8d50f06242c2e66d55b7bc6d13c
Marlo Kessler <marlo.kessler@connapptivity.de>
2021-03-23 16:52:44 +0100

    Some stuff

--- src/apps/LoginView/index.js
+++ src/apps/LoginView/index.js
@@ -5,32 +5,27 @@ import { Link } from "react-router-dom"
 import { FiChevronRight } from "react-icons/fi"
 
 
-function LoginView(){
-    return(
-      <div className="login">
-        <PageTitleElement>Log in to your existing account! 🎉</PageTitleElement>
-          <div>
-                <CardElement>
-                    <PageTitleElement className="Log-in"><a>Log-in</a></PageTitleElement>
-                    <label for="uname" className="labelUN"><b>E-Mail:</b></label><br/>
-                    <input  class="un " align="center" type="text" placeholder="Enter E-Mail" name="uname" required></input>
-                    <br/>
-                    <br/>
-                    <label for="psw" className="labelPass"><b>Password:</b></label><br/>
-                    <input class="pass" align="center" type="password" placeholder="Enter Password" name="psw" required></input>
-                </CardElement>  
-          </div>
-            <Link to="/">
-                <div className="toobar">
-                <button className="button btn-dark" type="submit">Login<FiChevronRight/></button>
-                </div>
-            </Link>
+export default function LoginView(){
+  return(
+    <div className="login">
+      <PageTitleElement>Log in to your existing account! 🎉</PageTitleElement>
+      <div>
+        <CardElement>
+          <PageTitleElement className="Log-in"><a>Log-in</a></PageTitleElement>
+          <label for="uname" className="labelUN"><b>E-Mail:</b></label><br/>
+          <input class="un " align="center" type="text" placeholder="Enter E-Mail" name="uname" required></input>
+          <br/>
+          <br/>
+          <label for="psw" className="labelPass"><b>Password:</b></label><br/>
+          <input class="pass" align="center" type="password" placeholder="Enter Password" name="psw" required></input>
+        </CardElement>  
       </div>
-    )
-  }
-
-  
-  export default LoginView
-
-
+      <Link to="/">
+        <div className="toobar">
+          <button className="button btn-dark" type="submit">Login<FiChevronRight/></button>
+        </div>
+      </Link>
+    </div>
+  )
+}
   
