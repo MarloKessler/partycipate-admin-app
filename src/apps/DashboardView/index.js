70568e235067f741e5a62179d18f80f333373131
Melanie-31 <melanie-rudloff@gmx.de>
2021-03-29 17:26:16 +0200

    DesignUpdates in DashboardView, AccountView

--- src/apps/DashboardView/index.js
+++ src/apps/DashboardView/index.js
@@ -6,14 +6,14 @@ import { Link } from "react-router-dom"
 
 export default function DashboardView(){
   return(
-    <div className="dashbard-view">
+    <div className="dashboard-view">
       <PageTitleElement>Welcome back, Partycipant!🎉</PageTitleElement>
-      <p className="hello">What do you want to do today?</p>
+      <div className="hello">What do you want to do today?</div>
       <div className="dv-cards">
-        <Link className='dv-item' to="/create-survey" ><CardElement> Create a brandnew Survey ❔</CardElement></Link>
-        <Link className='dv-item' to="/surveys"><CardElement>View Results of your Surveys 📊</CardElement></Link>
-        <Link className='dv-item' to="/my-account"><CardElement>Change Settings of your Account 🙂</CardElement></Link>
-        <Link className='dv-item' to="/docs"><CardElement>Get Help in our FAQs 🤝</CardElement></Link>
+        <Link className='dv-item' to="/create-survey" ><CardElement className="element"> Create a brandnew Survey ❔</CardElement></Link>
+        <Link className='dv-item' to="/surveys"><CardElement className="element">View Results of your Surveys 📊</CardElement></Link>
+        <Link className='dv-item' to="/my-account"><CardElement className="element">Change Settings of your Account 🙂</CardElement></Link>
+        <Link className='dv-item' to="/docs"><CardElement className="element">Get Help in our FAQs 🤝</CardElement></Link>
       </div>
     </div>
   )
