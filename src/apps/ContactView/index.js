9d59a4cd62ba74ad1b748479729dd0e458912b52
laura <wiu19186@lehre.dhbw-stuttgart.de>
2021-04-01 10:51:29 +0200

    Update contactview

--- src/apps/ContactView/index.js
+++ src/apps/ContactView/index.js
@@ -6,17 +6,46 @@ import { IoHelpOutline } from "react-icons/io5"
 import {AiOutlineMail} from "react-icons/ai"
 
 
-export default function ContactView() {
-  let iconStyles = { color: "black", fontSize: "3.0em" };
+export default function ContactView(){
+  let iconStyles = { color: "#647374", fontSize: "3.0em" };
   return(
     <div className="contact">
-      
-      <div className="left">
-        <PageTitleElement>Do you need any help ❔ </PageTitleElement>
-        <div>We are happy to help you with  setting up your account, finding the survey that fits to your question or any other sorts of issues. </div>
+      <div className="leftcontact">
+        <PageTitleElement className="pageelementcontact">Do you need any help ❔ </PageTitleElement>
+        <p>We are happy to help you with  setting up your account, finding the survey that fits to your question or any other sorts of issues.</p>
       </div>
 
-      <div className="right">
+      <div className="rightcontact">
+        <CardElement className="cardelementcontact">
+          <Link to="/" className="link">
+              <div >
+                <AiOutlineMail style={iconStyles}/>
+                <p>info@partycipate.de</p>
+              </div>
+          </Link>
+          
+          <Link to="/docs" className="link" >
+              <div >
+                <IoHelpOutline style={iconStyles}/>
+                <p>FAQs</p>
+              </div>
+          </Link>
+        </CardElement>
+      </div>
+    </div>
+  )
+}
+
+/*export default function ContactView() {
+  let iconStyles = { color: "black", fontSize: "3.0em" };
+  return(
+    <div className="contact"> 
+      <div className="leftcontact">
+        <PageTitleElement>Do you need any help ❔ </PageTitleElement>
+        <p>We are happy to help you with  setting up your account, finding the survey that fits to your question or any other sorts of issues. </p>
+      </div>
+
+      <div className="rightcontact">
           <CardElement className="contactBox"> 
             
             <Link to="/" className="link">
@@ -38,18 +67,4 @@ export default function ContactView() {
     </div>
   )
 }
-
-
-const Bare = () => {
-  return (
-    <div>
-      <IoHelpOutline />
-    </div>
-  )
-}
-
-/*class Question extends React.Component {
-  render() {
-    return <IoHelpOutline /> 
-  }
-}*/
+*/
