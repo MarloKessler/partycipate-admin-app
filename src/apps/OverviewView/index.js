c2d981a5551e613be4e195860c192e26a4a5485c
Melanie-31 <melanie-rudloff@gmx.de>
2021-04-22 18:34:43 +0200

    OverviewView

---
+++ src/apps/OverviewView/index.js
@@ -0,0 +1,110 @@
+import "./style.css"
+import { useEffect, useState } from "react"
+import { useHistory } from "react-router-dom"
+import Server from "../Server"
+import StandardPage from "../StandardPage"
+import ErrorPage from "../ErrorPage"
+import CardElement from "../CardElement"
+import { HelpSections } from "../DocsView"
+import CrossSurveyResultsCard from "../CrossSurveyResultsCard"
+
+
+function SurveyOverview() {
+    const handleFilter = (survey, searchInput) => survey.title.toLowerCase().includes(searchInput)
+    return (
+        <OverviewView onFilter={handleFilter}/>
+    )
+}
+
+
+export default function OverviewView({ containerClassName="", title, helpSection, className="" }) {
+    
+    const [surveys, setSurveys] = useState()
+
+    useEffect(() => {
+        Server.database().getSurveys()
+        .then(setSurveys)
+        .catch(() => setSurveys(null))
+    }, [])
+
+    return (
+        <StandardPage className={`overview-view ${containerClassName}`} helpSection={helpSection} title={title}>    
+            <CrossSurveyResultsCard className="so-csrc"/>
+            { surveys
+                ? <Surveys surveys={surveys}/>
+                : surveys === null && <ErrorPage message="We are sorry, your surveys couldn't be loaded!"/>
+            }
+        </StandardPage>
+    )
+}
+
+function Surveys({surveys, onFilter}) {
+    const history = useHistory()
+    const [filteredSurveys, setFilteredSurveys] = useState(surveys)
+
+    const handleSearchInput = event => {
+        const searchInput = event.target.value.toLowerCase()
+        if (searchInput === "") setFilteredSurveys(surveys)
+        else {
+            const filteredArray = surveys.filter(survey => onFilter(survey, searchInput))
+            setFilteredSurveys(filteredArray)
+        }
+    }
+
+    return (
+        <div>
+            <div className="so-search-bar-container">
+                <CardElement className="so-search-bar"><input type="text" placeholder="xyz"></input></CardElement>
+            </div>
+            <ul className="survey-list">
+                { filteredSurveys &&
+                    filteredSurveys.map( survey => (
+                        <li key={survey.id}>
+                            <CardElement className="link light primary-element so-card" onClick={() => history.push(`/surveys/${survey.id}`)}>{survey.title}</CardElement>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+
+/*return (
+    <div className={`standard-page ${containerClassName}`}>
+        { title && <PageTitleElement className="sp-title" helpSection={helpSection}>{ title }</PageTitleElement> }
+        <div className={`sp-body ${className}`}>
+            { children }
+        </div>
+    </div>
+)*/
+
+
+function Surveys({surveys}) {
+    const history = useHistory()
+    const [filteredSurveys, setFilteredSurveys] = useState(surveys)
+    
+    const handleSearchInput = event => {
+        const searchInput = event.target.value.toLowerCase()
+        if (searchInput === "") setFilteredSurveys(surveys)
+        else {
+            const filteredArray = surveys.filter(survey => survey.title.toLowerCase().includes(searchInput))
+            setFilteredSurveys(filteredArray)
+        }
+    }
+
+    return (
+        <div>
+            <div className="so-search-bar-container">
+                <CardElement className="so-search-bar"><input type="text" placeholder="Search" onInput={handleSearchInput}/></CardElement>
+            </div>
+            <ul className="survey-list">
+                { filteredSurveys &&
+                    filteredSurveys.map( survey => (
+                        <li key={survey.id}>
+                            <CardElement className="link-light primary-element so-card" onClick={() => history.push(`/surveys/${survey.id}`)}>{survey.title}</CardElement>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
