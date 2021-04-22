b489797a562d0d1d9b7ad302f705d33a3cd30399
Marlo Kessler <marlo.kessler@connapptivity.de>
2021-04-22 18:35:14 +0200

    Updates in Main and Server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from "react-router-dom"
+import {  } from "react-router/lib/"
 import Main from './apps/Main'
-
 
 ReactDOM.render(
   <React.StrictMode>
